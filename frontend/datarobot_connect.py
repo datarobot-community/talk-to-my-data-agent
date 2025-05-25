c1ef3f42f27c4b28e1a7006d2fdfacf4d2814d51
ironerumi <39033099+ironerumi@users.noreply.github.com>
2025-05-25 21:08:46 +0900

    generalize message related for input api token

--- frontend/datarobot_connect.py
+++ frontend/datarobot_connect.py
@@ -161,7 +161,7 @@ class DataRobotTokenManager:
             stc.write(f"Hello {username}")
 
         if not self._user_creds.token:
-            stc.warning("Data Registry disabled. Please provide your API token")
+            stc.warning("Please provide your API token")
             token = stc.text_input(
                 "API Token",
                 key="datarobot_api_token_provided",
